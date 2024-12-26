e4a852503a4f25ae5f8bb635c86d7e3f628556ea
jash chauhan <139748154+1223323@users.noreply.github.com>
2024-12-26 16:28:34 +0530

    Update App.js

--- FRONTEND/src/App.js
+++ FRONTEND/src/App.js
@@ -1,13 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import { Navbar} from './Component/Navbar/Navbar';
-
+import { Navbar } from './Component/Navbar/Navbar';
+import { Home } from './Component/Home/Home';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { darkTheme } from './Theme/DarkTheme';
+import  RestaurantDetails  from './Component/Restaurant/RestaurantDetails';
+import  {Cart}  from './Component/Cart/Cart';
+import  Profile  from './Component/Profile/Profile';
+import CustomerRouter from './Component/Routers/CustomerRouter';
+import { Auth } from './Component/Auth/Auth';
 function App() {
   return (
-    <div >
-     
-     <Navbar/>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+
+      <CssBaseline />
+
+      <Navbar />
+      {/* <Home /> */}
+      {/* <RestaurantDetails /> */}
+      {/* <Cart/> */}
+      {/* <Profile />  */}
+      <CustomerRouter/>
+      
+    </ThemeProvider>
   );
 }
 
